75f84962a44a7f004702b9277ee7ce27d88049b6
saniya-ahuja <saniyaahuja383@gmail.com>
2026-01-15 12:01:03 +0530

    Initial commit

---
+++ myapp.py
@@ -0,0 +1,33 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock **closing price** and ***volume*** of Google!
+""")
+
+# Define the ticker symbol
+tickerSymbol = 'GOOGL'
+
+# Get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+# Get historical prices
+# ❌ Removed 'period' (cannot be used with start & end)
+tickerDf = tickerData.history(
+    start='2010-05-31',
+    end='2020-05-31'
+)
+
+# Display closing price
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf['Close'])
+
+# Display volume
+st.write("""
+## Volume
+""")
+st.line_chart(tickerDf['Volume'])
